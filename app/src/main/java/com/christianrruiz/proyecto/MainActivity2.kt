82ee77bad6392d3d3bfdf367e221f9eb95639cf0
CHRISTIAN RUIZ <christianr_ruiz@yahoo.com>
2021-11-06 22:02:07 -0500

    Initial commit

---
+++ app/src/main/java/com/christianrruiz/proyecto/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.christianrruiz.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+ //       val btn = findViewById<Button>(R.id.btn)
+ //       btn.setOnClickListener {
+ //           startActivity(intent)
+ //       }
+    }
+
+}
