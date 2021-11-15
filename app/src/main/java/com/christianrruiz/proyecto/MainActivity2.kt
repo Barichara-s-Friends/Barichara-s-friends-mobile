f3df450e15aab966e4f2fb1905b3d9d094ee59c2
Luis Munoz <luisalejandromunozsierra@gmail.com>
2021-11-15 00:38:53 -0300

    Sprint1 feature interface integration

---
+++ app/src/main/java/com/christianrruiz/proyecto/MainActivity2.kt
@@ -0,0 +1,48 @@
+package com.christianrruiz.proyecto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+//        val boton30=findViewById<ImageButton>(R.id.imageButton3)
+//        boton30.setOnClickListener {
+//            val intent = Intent(this, MainActivity5::class.java)
+//            startActivity(intent)
+//        }
+
+ //       val boton6 =findViewById<ImageButton>(R.id.imageButton6)
+ //       boton6.setOnContextClickListener { val intent = Intent(this, MainActivity::class.java)
+ //           startActivity(intent)}
+
+        val btn = findViewById<ImageButton>(R.id.imageButton6)
+        btn.setOnClickListener {
+            val intent8 = Intent(this, MainActivity7::class.java)
+            startActivity(intent8)
+        }
+
+        val salir = findViewById<ImageButton>(R.id.imageButton2)
+        salir.setOnClickListener {
+            val intent5 = Intent(this, MainActivity3::class.java)
+            startActivity(intent5)
+        }
+
+        val botonsalir = findViewById<ImageButton>(R.id.imageButton4)
+        botonsalir.setOnClickListener {
+            val intent3 = Intent(this, MainActivity::class.java)
+            startActivity(intent3)
+        }
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/christianrruiz/proyecto/MainActivity3.kt
@@ -0,0 +1,69 @@
+package com.christianrruiz.proyecto
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity3 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toogle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toogle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toogle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.nav_item_one -> Toast.makeText(this, "item 1", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_two -> Toast.makeText(this, "item 2", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_three -> Toast.makeText(this, "item 3", Toast.LENGTH_SHORT).show()
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toogle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toogle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toogle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
