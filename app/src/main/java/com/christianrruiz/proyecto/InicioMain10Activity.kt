03d22723fbdf58021729aba4517390916aade87f
vivian walls <doly8328@gmail.com>
2021-11-22 00:38:32 -0500

    creacion de las interfaces de usuario de inicio, registro,login, ingreso, sitio salto del mico.

---
+++ app/src/main/java/com/christianrruiz/proyecto/InicioMain10Activity.kt
@@ -0,0 +1,11 @@
+package com.christianrruiz.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioMain10Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_main10)
+    }
+}
---
+++ app/src/main/java/com/christianrruiz/proyecto/MainActivity13.kt
@@ -0,0 +1,11 @@
+package com.christianrruiz.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity13 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_salto_del_mico_main13)
+    }
+}
